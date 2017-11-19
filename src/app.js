7003992a6322f882c6f5a1f63739ef36bf6f3ff5
Donald Westland <don@Dons-MacBook-Pro.local>
2017-11-18 19:49:14 -0800

    Completed Course 30

--- src/app.js
+++ src/app.js
@@ -26,18 +26,30 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
     render() {
         return (
             <div>
-                <button>What should I do?</button>
+                <button onClick={this.handlePick}>What should I do?</button>
             </div>
         )
     }
 }
 class Options extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+    
+    handleRemoveAll () {
+        console.log(this.props.options)
+    }
     render() {
         return (
             <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     this.props.options.map((option) => <Option key={option} optionText={option} />)
                 }
@@ -57,10 +69,20 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+        if(option) {
+            alert(option)
+        }
+    }
     render() {
         return (
             <div>
-            <p>AddOption component here</p>
+                <form onSubmit={this.handleAddOption}>
+                    <input type="text" name="option"/>
+                    <button>Add Option</button>
+                </form>
             </div>
         )
     }
